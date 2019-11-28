9e5d6cc9bf2729005dd99c5c837b6e8339052f8f
Avner <avner.conceicao@hotmail.com>
2019-11-28 00:38:09 -0300

    Criando primeiros passos do jogo em 'Round'

--- src/components/Round/Round.js
+++ src/components/Round/Round.js
@@ -7,7 +7,8 @@ import styled from 'styled-components'
 import Dice20Faces from '../../assets/images/dice-20-faces.png'
 
 //Actions
-import { SET_DICE_PLAYER_ASYNC, SET_DRAW_PLAYER_ASYNC, SET_ROUND_AND_PLAYER_ASYNC, SET_TURN_PLAYER_ASYNC } from '../../actions/saga/round'
+import { SET_WHOS_ROUND, CLEAN_WHOS_ROUND } from '../../actions/round'
+import { SET_ROUND_ASYNC } from '../../actions/saga/round'
 
 //Styled Components
 const Img = styled.img`
@@ -36,214 +37,180 @@ const ContentCenterRow = styled.div`
 class Round extends PureComponent
 {
 	state = {
+		isDiceLock: true,
+		isBattleGame: false,
 		messageRound: '',
-		valueDice: 0,
-		isStartedGame: false,
-		isStartedRound: false,
+		diceValue: 0,
+		diceOrc: 0,
+		diceHuman: 0,
+		isOrcPlayed: false,
+		isHumanPlayed: false,
 		dataOrc: [],
 		dataHuman: [],
-		player: '',
+		player: ''
 	}
 
-	firstStep = _ => {
-		const { isStartedRound } = this.state
-		const { initialPlayer, isHumanPlayed, isOrcPlayed } = this.props
+	componentDidUpdate() {
+		const { dataOrc, dataHuman } = this.state
+		const { dataPlayers } = this.props
 
-		if (!isStartedRound) {
-			if (initialPlayer === 0) {
-				if (isHumanPlayed && !isOrcPlayed) {
-					this.setState({
-						player: 'orc',
-						messageRound: 'Início de jogo!\nJogador \'Orc\' rode o dado!',
-						valueDice: 0
-					})
-				}
-
-				if (!isHumanPlayed && isOrcPlayed) {
-					this.setState({
-						player: 'human',
-						messageRound: 'Início de jogo!\nJogador \'Human\' rode o dado!',
-						valueDice: 0
-					})
-				}
-
-				if (isHumanPlayed && isOrcPlayed) {
-					this.checkHighestValue()
-				}
-			}
-		} else {
-			this.secondStep()
+		if (dataOrc.length === 0 && dataHuman.length === 0 && dataPlayers.length > 0) {
+			this.setState({
+				isDiceLock: false,
+				dataOrc: dataPlayers[0].nome === 'orc' ? dataPlayers[0] : dataPlayers[1],
+				dataHuman: dataPlayers[0].nome === 'human' ? dataPlayers[0] : dataPlayers[1]
+			}, _=> this.firstStep('human'))
 		}
 	}
 
-	checkHighestValue = _ => {
-		const { dispatch, diceHumanPlayer, diceOrcPlayer, initialPlayer, gameId } = this.props
-		const { dataOrc, dataHuman } = this.state
-
-		//Check if draw
-		if ((diceHumanPlayer + dataHuman.agilidade) === (diceOrcPlayer + dataOrc.agilidade)) {
-			dispatch({
-				type: SET_DRAW_PLAYER_ASYNC
-			})
-
-			let player = dataHuman.nome
-			if (initialPlayer > 0) {
-				player = initialPlayer === dataHuman.id ? dataHuman.nome : dataOrc.nome
-			}
-
+	firstStep = async (player) => {
+		const { dispatch, initialPlayer } = this.props
+		let { orcId, humanId } = this.props
+		const { isHumanPlayed, isOrcPlayed, diceHuman, diceOrc, dataOrc, dataHuman } = this.state
+		let playerName = player === 'human' ? 'Humano' : 'Orc'
+		
+		if (isOrcPlayed && isHumanPlayed) {
 			this.setState({
-				player: player,
-				messageRound: `Deu empate!\nJogador\'${player}\' jogue novamente os dados!`,
+				messageRound: 'Verificando resultados....',
+				diceValue: 0
+			}, _ => {
+				if (orcId === 0 || humanId === 0) {
+					orcId = dataOrc.id
+					humanId = dataHuman.id
+				}
+	
+				const sumHuman = diceHuman + dataHuman.agilidade
+				const sumOrc = diceOrc + dataOrc.agilidade
+	
+				//Draw
+				if (sumHuman === sumOrc) {
+					let toPlay = 'human'
+	
+					if (initialPlayer > 0 && initialPlayer === orcId)
+						toPlay = 'orc'
+	
+					this.setState({
+						isHumanPlayed: false,
+						isOrcPlayed: false,
+						diceHuman: 0,
+						diceOrc: 0,
+						player: toPlay,
+						messageRound: `Deu empate!\nJogador\'${playerName}\' jogue novamente os dados!`,
+					}, _=> dispatch({
+						type: CLEAN_WHOS_ROUND
+					}))
+	
+					return 
+				}
+	
+				//Highest value
+				let idPlayer = humanId
+				let toPlay = 'human'
+	
+				if (sumOrc > sumHuman) {
+					idPlayer = orcId
+					toPlay = 'orc'
+				}
+	
+				playerName = toPlay === 'human' ? 'Humano' : 'Orc'
+	
+				//Set content showed
+				this.setState({
+					messageRound: `O Jogador \'${playerName}\' começa o turno!`,
+					diceValue: 0,
+					diceOrc: 0,
+					diceHuman: 0,
+					isOrcPlayed: false,
+					isHumanPlayed: false,
+					player: toPlay,
+					isDiceLock: true,
+				}, _=> new Promise((resolve) => {
+					//Set Initial Round
+					dispatch({
+						type: SET_ROUND_ASYNC,
+						payload: {
+							idPlayer
+						}
+					})
+	
+					resolve()
+				})
+				.then(_ => {
+					this.setState({
+						messageRound: `O Jogador \'${playerName}\' role o dado!`,
+						isDiceLock: false,
+						player: toPlay,
+						isBattleGame: true
+					})
+				})
+				.then(_=> dispatch({
+					type: SET_WHOS_ROUND,
+					payload: {
+						player: toPlay
+					}
+				})))
 			})
-
+			
 			return
 		}
 
-		//Check if highest
-		const starterPlayer = (diceHumanPlayer + dataHuman.agilidade) > (diceOrcPlayer + dataOrc.agilidade) ? dataHuman : dataOrc
-		
-		dispatch({
-			type: SET_ROUND_AND_PLAYER_ASYNC,
-			payload: {
-				initialPlayer: starterPlayer.id,
-				gameId
-			}
-		})
-
-		//Set begining of round
 		this.setState({
-			isStartedRound: true,
-			valueDice: 0,
-			messageRound: '',
-			player: ''
-		})
-
-		//Call second step
-		this.secondStep()
-	}
-
-	secondStep = _ => {
-		const { isStartedRound, player, dataOrc, dataHuman } = this.state
-		const { initialPlayer, isHumanPlayed, isOrcPlayed, diceHumanPlayer, diceOrcPlayer } = this.props
-
-		if (isStartedRound) {
-			//Check if played(initialPlayer)
-			const whoInitialPlayer = initialPlayer === dataHuman.id ? dataHuman.nome : dataOrc.nome
-
-			if (whoInitialPlayer === 'human') {
-				this.actionStep(dataHuman, dataOrc, isHumanPlayed, isOrcPlayed, diceHumanPlayer, diceOrcPlayer)
-			} else {
-				this.actionStep(dataOrc, dataHuman, isOrcPlayed, isHumanPlayed, diceOrcPlayer, diceHumanPlayer)
-			}
-		}
-	}
-
-	actionStep = (atkPlayer, defPlayer, firstPlayed, secondPlayed, atkDice, defDice) => {
-		if (firstPlayed && !secondPlayed) {
-			this.setState({
-				player: defPlayer.nome,
-				messageRound: `Jogador \'${defPlayer.nome}\' rode os dados para se defender!`,
-				valueDice: 0
-			})
-		}
-
-		if (!firstPlayed && !secondPlayed) {
-			this.setState({
-				player: atkPlayer.nome,
-				messageRound: `Jogador \'${atkPlayer.nome}\' rode os dados para atacar!`,
-				valueDice: 0
-			})
-		}
-
-		if (firstPlayed && secondPlayed) {
-			const atk = atkDice + atkPlayer.arma.forca + atkPlayer.agilidade
-			const def = defDice + defPlayer.arma.defesa + defPlayer.agilidade
-
-			if (atk === def) {
-				this.setState({
-					player: defPlayer.nome
-				})
-
-				this.secondStep()
-				return 
-			}
-
-			if (atk > def) {
-				this.setState({
-					messageRound: `Jogador \'${atkPlayer.nome}\' rode o seu dado para calcular o ataque!`,
-					valueDice: 0
-				})
-
-				dispatch({
-					type: SET_ROUND_DAMAGE,
-					payload: {
-						player: atkPlayer.nome
-					}
-				})
-			}
-		}
-		
-	}
-
-	//Jogar na saga
-	throwDamage = (valueDice, atkPlayer, defPlayer) => {
-		const damage = valueDice + atkPlayer.arma.forca
-		dispatch({
-			type: SET_DAMAGE_ON_DEFENDER,
+			messageRound: `Início de jogo!\nJogador \'${playerName}\' rode o dado!`,
+			player
+		}, _=> dispatch({
+			type: SET_WHOS_ROUND,
 			payload: {
-				id: defPlayer.id,
-				damage
+				player
 			}
-		})
+		}))
 	}
 
+	secondStep = player => {
+		/*
+		const { dispatch, initialPlayer } = this.props
+		let { orcId, humanId } = this.props
+		const { isHumanPlayed, isOrcPlayed, diceHuman, diceOrc, dataOrc, dataHuman } = this.state
+		let playerName = player === 'human' ? 'Humano' : 'Orc'*/
 
-	componentDidUpdate() {
-		const { isStartedGame } = this.state
-		const { dataPlayers } = this.props
 
-		if (!isStartedGame && dataPlayers.length > 0) {
-			this.setState({
-				isStartedGame: true,
-				messageRound: 'Início de jogo!\nJogador \'Humano\' rode o dado!',
-				player: 'human',
-				idOrc: dataPlayers[0].nome === 'orc' ? dataPlayers[0] : dataPlayers[1],
-				idHuman: dataPlayers[0].nome === 'human' ? dataPlayers[0] : dataPlayers[1]
-			})
-		}
 	}
 
 	throwDices = _ => {	
-		const { isStartedGame, player } = this.state
-		const { dispatch, isRoundDamage } = this.props
-
-		if (isStartedGame && isRoundDamage.length === 0) {
+		const { isDiceLock, diceHuman, diceOrc, player, isHumanPlayed, isOrcPlayed, isBattleGame } = this.state
+	
+		if (!isDiceLock) {
 			const min = Math.ceil(1);
 			const max = Math.floor(20);
-			const valueDice = Math.floor(Math.random() * (max - min + 1)) + min
-
+			const diceValue = Math.floor(Math.random() * (max - min + 1)) + min
+			const throwDiceOrc = player === 'orc' ? diceValue : diceOrc
+			const throwDiceHuman = player === 'human' ? diceValue : diceHuman
+	
 			//Set value of dice
-			this.setState({ 
-				valueDice 
-			})
+			this.setState({
+				diceValue,
+				diceOrc: throwDiceOrc,
+				diceHuman: throwDiceHuman,
+				isHumanPlayed: player === 'human' ? true : isHumanPlayed,
+				isOrcPlayed: player === 'orc' ? true : isOrcPlayed
+			}, _=> {
+				//Call start round
+				!isBattleGame && this.firstStep(player === 'orc' ? 'human': 'orc')
 
-			//Set Dice Player
-			dispatch({
-				type: SET_DICE_PLAYER_ASYNC,
-				payload: {
-					player,
-					valueDice
-				}
+				//Call battle
+				isBattleGame && this.secondStep(player === 'orc' ? 'human': 'orc')
 			})
-			
-			//Call start round
-			this.firstStep()
 		}
 	}
+	
 
 	render() {
-		const { messageRound, valueDice } = this.state
-		const { round } = this.props
+		const { messageRound, diceValue } = this.state
+		const { round, error } = this.props
+
+		if (error)
+			return (
+				<Row>{error}</Row>
+			)
 
 		return (
 			<ContentCenterRow>
@@ -273,7 +240,7 @@ class Round extends PureComponent
 						<Form.Control 
 							type='text'
 							ref='txtDice'
-							value={valueDice > 0 ? valueDice : ''}
+							value={diceValue > 0 ? diceValue : ''}
 							readOnly
 						/>
 					</Col>
